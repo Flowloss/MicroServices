439de4aa9e3e77d1fe82160174ae0aae7a0b6011
Flowloss <kolipopan@gmail.com>
2024-06-20 12:52:31 +0200

    Fixat eureka server/client

--- src/main/java/jacob/wigellspadel/WigellsPadelApplication.java
+++ src/main/java/jacob/wigellspadel/WigellsPadelApplication.java
@@ -3,9 +3,9 @@ package jacob.wigellspadel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class WigellsPadelApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WigellsPadelApplication.class, args);
     }
